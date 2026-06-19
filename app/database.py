dec6f7609e72ace1dd8cef7f00dc765b1db584cc
RodrigoSBA14 <rodrigobrena2122@gmail.com>
2026-06-19 12:00:46 -0600

    Preparado par subir a vercel

--- app/database.py
+++ app/database.py
@@ -1,12 +1,16 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./fc_career.db"
+load_dotenv()
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise Exception("No se encontró la variable DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
---
+++ import_data.py
@@ -0,0 +1,65 @@
+from app.database import SessionLocal
+from app import models
+import json
+
+db = SessionLocal()
+
+with open(
+    "fc_career_export.json",
+    "r",
+    encoding="utf-8"
+) as archivo:
+    data = json.load(archivo)
+
+print("Importando equipos...")
+
+for item in data.get("equipos", []):
+    db.merge(models.Equipo(**item))
+
+db.commit()
+
+print("Importando carreras...")
+
+for item in data.get("carreras", []):
+    db.merge(models.Carrera(**item))
+
+db.commit()
+
+print("Importando temporadas...")
+
+for item in data.get("temporadas", []):
+    db.merge(models.Temporada(**item))
+
+db.commit()
+
+print("Importando jugadores...")
+
+for item in data.get("jugadores", []):
+    db.merge(models.Jugador(**item))
+
+db.commit()
+
+print("Importando jugadores_temporada...")
+
+for item in data.get("jugadores_temporada", []):
+    db.merge(models.JugadorTemporada(**item))
+
+db.commit()
+
+print("Importando estadisticas_club...")
+
+for item in data.get("estadisticas_club", []):
+    db.merge(models.EstadisticaClub(**item))
+
+db.commit()
+
+print("Importando logros_club...")
+
+for item in data.get("logros_club", []):
+    db.merge(models.LogroClub(**item))
+
+db.commit()
+
+db.close()
+
+print("Importación completada correctamente.")
